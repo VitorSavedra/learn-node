0db823ce75f11efe9036d0ba3cc0377c4c0e71b9
vitorsavedra <vitor-savedra@outlook.com>
2017-06-18 16:39:24 -0300

    Validação, cadastro via JSON e +

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -21,11 +21,22 @@ module.exports = function (app) {
     app.get('/produtos', listaProdutos);
 
     app.get('/produtos/form', function (req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form',
+        {errosValidacao:{},produto:{}});
     });
 
     app.post('/produtos', function (req, res) {
         var produto = req.body;
+        
+        req.assert('titulo','Título obrigatório').notEmpty();
+        req.assert('preco','Formato inválido').isFloat();
+        
+        var erros = req.validationErrors();
+        if(erros){
+            res.render('produtos/form',{errosValidacao:erros,produto:produto});
+            return;
+        }
+        
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
