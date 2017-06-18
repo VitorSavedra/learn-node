cf687e6bb24def3284f0c217b0b1b456563e6ced
vitorsavedra <vitor-savedra@outlook.com>
2017-06-18 18:23:51 -0300

    Aplicação de testes.

---
+++ test/produtos.js
@@ -0,0 +1,39 @@
+var express = require('../config/express')();
+var request = require('supertest')(express);
+
+describe('#ProdutosController', function () {
+    it('#Listagem JSON', function (done) {
+        request.get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('Content-type', /json/)
+            .expect(200, done);
+    });
+
+    it('#Listagem HTML', function (done) {
+        request.get('/produtos')
+            .set('Accept', 'text/html')
+            .expect('Content-type', /html/)
+            .expect(200, done);
+    });
+
+    it('#Cadastro de novo produto com dados inválidos', function (done) {
+        request.post('/produtos')
+            .send({
+                titulo: '',
+                preco: '100,30',
+                descricao: 'Novo livro'
+            })
+            .expect(400, done);
+    });
+
+    it('#Cadastro de novo produto com dados válidos', function (done) {
+        request.post('/produtos')
+            .send({
+                titulo: 'Título Teste',
+                preco: 100.50,
+                descricao: 'Novo livro'
+            })
+            .expect(302, done);
+    });
+
+});
